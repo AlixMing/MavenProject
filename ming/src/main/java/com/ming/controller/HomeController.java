12a6d38f03b0c5fe012541efd556fc1d256ea371
Alix <Alix_huang@sina.cn>
2015-11-25 17:29:02 +0800

    something

--- ming/src/main/java/com/ming/controller/HomeController.java
+++ ming/src/main/java/com/ming/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.ming.controller;
 
 import com.jfinal.core.Controller;
 import com.jfinal.ext.route.ControllerBind;
-import com.jfinal.kit.JsonKit;
+import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Page;
 import com.ming.model.User;
 
@@ -11,16 +11,21 @@ import com.ming.model.User;
  */
 @ControllerBind(controllerKey="/",viewPath="/home")
 public class HomeController extends Controller {
+    private static final Logger logger = Logger.getLogger(HomeController.class); 
+    
 	public void index() {
 		renderFreeMarker("index.ftl");
 	}
 	
 	public void login(){
+	    String name = getPara("user_name");
+	    String password = getPara("user_password");
+	    System.out.println("name:" + name);
+	    System.out.println("password:" + password);
 	    Page<User> userList = User.me.paginate(1, 5); 
-	    System.out.println(userList.getList().size());
+	    logger.debug("userList size:" + userList.getList().size());
 	    setAttr("userList", userList.getList());
-	    System.out.println("......");
-	    renderJson(JsonKit.toJson("hello"));
+	    renderFreeMarker("login.ftl");
 	}
 }
 
